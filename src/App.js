87d498edc87f7d26b0859bea6a124527fafb6b55
Raymond Lin <rlin264@gmail.com>
2020-03-14 17:09:11 -0400

    packages and other

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // import Navbar from "./components/navbar.component";
 // import CitiesList from "./components/cities-list.component"
 import Map from "./components/map.component";
+import News from "./components/news.component";
 
 function App() {
     return(
         <Router>
             <Map/>
+            <News/>
         </Router>
     )
 }
