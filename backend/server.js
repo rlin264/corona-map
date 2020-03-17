3807931db02c235a520f8b78dc4e3e6bcdb82d19
Raymond Lin <rlin264@gmail.com>
2020-03-17 16:21:02 -0400

    heroku again

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, '../')));
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname + "../build/index.html"));
