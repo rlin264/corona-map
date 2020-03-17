c29252454b7f50a393fb0bdb95de045464b76465
Raymond Lin <rlin264@gmail.com>
2020-03-16 21:44:28 -0400

    fixed upsertion

--- backend/routes/places.js
+++ backend/routes/places.js
@@ -1,39 +1,52 @@
 const express = require('express');
 const Place = require('../models/place.model');
 // const {getPlaces, addPlace} = require('../controllers/places');
+const geoCoder = require('../utils/geocoder')
 
 const router = express.Router();
 
-router.route('/').post((req, res) => {
+router.route('/').post(async(req, res) => {
     const _id = req.body.address;
     const address = req.body.address;
     const cases = req.body.cases;
     const filter = {_id: _id};
-    const newPlace = new Place({
-        _id,
-        address,
-        cases
-    });
-    const update = {
-        _id: _id,
-        address: address,
-        cases: cases,
-    }
-    Place.findOneAndUpdate(filter, update, {upsert: true}, function(err, doc) {
-        if (err) return res.send(500, {error: err});
-        return res.send('Succesfully saved.');
-    });
-    // var upsertData = newPlace.toObject();
-    // delete upsertData._id;
-    // newPlace.update({_id:newPlace._id}, upsertData,  {upsert: true}, function(err, doc){
-    //     if(err) throw err;
-    //     console.log(doc);
-    // });
-    // newPlace.save()
-    //     .then(()=> res.json('Place added'))
-    //     .catch(err => {
-    //         res.status(400).json('Error2: ' + err)
-    //     });
+    // const loc = await geoCoder.geocode(address);
+    // const location = {
+    //     type: 'Point',
+    //     coordinates: [loc[0].longitude, loc[0].latitude],
+    //     formattedAddress: loc[0].formattedAddress
+    // };
+
+    Place.count({_id:_id}, function(err, count){
+        if(count > 0){
+            const update = {
+                _id: _id,
+                address: address,
+                cases: cases,
+            }
+            Place.findOneAndUpdate(filter, update, function(err, doc) {
+                if (err){ 
+                    return res.send(500, {error: err});
+                }
+                return res.send('Succesfully saved.');
+            });
+        }
+        else{
+            const newPlace = new Place({
+                _id,
+                address,
+                cases
+            });
+
+            newPlace.save()
+            .then(()=> res.json('City added'))
+            .catch(err => {
+                console.log('place')
+                console.log(newPlace);
+                res.status(400).json('Error: ' + err)
+            });
+        }
+    })
 });
 
 router.route('/').get((req, res) =>{
@@ -41,5 +54,11 @@ router.route('/').get((req, res) =>{
         .then(places => res.json(places))
         .catch(err => res.status(400).json('Error3:' + err));
 });
+
+router.route('/:id').delete((req, res) => {
+    Place.findByIdAndDelete(req.params.id)
+        .then(()=>res.json('Places deleted'))
+        .catch(err => res.status(400).json('Error: ' + err));
+})
 
 module.exports = router;
