fbfa6cff027aed8f2ae65f40a681fc4c89791ea7
Raymond Lin <rlin264@gmail.com>
2020-03-17 15:46:25 -0400

    heroku react

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,11 @@ const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "../build/index.html"));
+});
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
