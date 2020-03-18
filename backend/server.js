3bfbb103c938a35efdf8935573350992778eac38
Raymond Lin <rlin264@gmail.com>
2020-03-17 20:54:32 -0400

    heroku pls

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,6 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../build')));
-// app.use(express.static(path.join(__dirname, '../public')));
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/../build/index.html"));
-});
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
@@ -32,6 +26,13 @@ const placeRouter = require('./routes/places');
 app.use('/news', newsRouter);
 app.use('/cities', cityRouter);
 app.use('/places', placeRouter);
+
+app.use(express.static(path.join(__dirname, '../build')));
+// app.use(express.static(path.join(__dirname, '../public')));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/../build/index.html"));
+});
 
 // Set static folder
 // app.use(express.static(path.join(__dirname, 'public')));
